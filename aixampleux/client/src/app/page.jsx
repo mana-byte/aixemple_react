175f8f85db19297cd4be5cfaba7d2f9cd897aaaf
mana-byte <manaikilaut@gmail.com>
2024-11-29 16:32:50 +0100

    added(global.css, homepage, postit): more robust code and more beautiful postit

--- aixampleux/client/src/app/page.jsx
+++ aixampleux/client/src/app/page.jsx
@@ -2,22 +2,53 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import PostIt from "./postit/postit";
 
+const postItCorrect = (postIt) => {
+    const template = {
+        id: 0,
+        title: "",
+        text: "",
+        subtitle: "",
+        links: [],
+    };
+    return {
+        id: typeof postIt.id === "number" ? postIt.id : template.id,
+        title: typeof postIt.title === "string" ? postIt.title : template.title,
+        text: typeof postIt.text === "string" ? postIt.text : template.text,
+        subtitle:
+            typeof postIt.subtitle === "string" ? postIt.subtitle : template.subtitle,
+        links: Array.isArray(postIt.links) ? postIt.links : template.links,
+    };
+};
+
 export default function Index() {
     const links = [
         { id: 2, url: "https://www.google.com", text: "Google" },
         { id: 3, url: "https://www.bing.com", text: "Bing" },
     ];
-    const postIt = [ 
-        { id:1, title: "Remember to check those web sites", links: links }
+    const postIt = [
+        { id: 1, title: "Remember to check those web sites", links: links },
+        { id: 2, title: "Remember to check those web sites", links: [] },
     ];
 
     return (
         <div>
-        <a href="https://www.google.com">Google</a>
-        { postIt.map((postIt) => {
-            console.log(postIt);
-            return (<PostIt key={postIt.id} title={postIt.title} links={postIt.links} />);
-        })}
+            <a href="https://www.google.com">Google</a>
+            {postIt.map((postIt) => {
+                const postItCorrected = postItCorrect(postIt);
+                try {
+                    return (
+                        <PostIt
+                            key={postItCorrected.id}
+                            title={postItCorrected.title}
+                            text={postItCorrected.text}
+                            subtitle={postItCorrected.subtitle}
+                            links={postItCorrected.links}
+                        />
+                    );
+                } catch (e) {
+                    console.log(e);
+                }
+            })}
         </div>
     );
 }
--- aixampleux/client/src/app/postit/postit.jsx
+++ aixampleux/client/src/app/postit/postit.jsx
@@ -3,7 +3,10 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 export default function PostIt(props) {
     return (
-        <div className="card" style={{ width: "18rem", backgroundColor: "yellow" }}>
+        <div
+            className="card postIt"
+            style={{ width: "18rem", backgroundColor: "yellow" }}
+        >
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
