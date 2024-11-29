b99197e6f554131f48c45e8568a03fcb0940b29b
mana-byte <manaikilaut@gmail.com>
2024-11-29 15:45:34 +0100

    added(page.jsx, postit.jsx): postit component and home page presentation

--- aixampleux/client/src/app/page.jsx
+++ aixampleux/client/src/app/page.jsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PostIt from "./postit/postit";
 
-export default function Home() {
-	return (
-		<div className={styles.container}>
-			<p className={styles.text}>Hello, world!</p>
-		</div>
-	);
+export default function Index() {
+    const links = [
+        { id: 2, url: "https://www.google.com", text: "Google" },
+        { id: 3, url: "https://www.bing.com", text: "Bing" },
+    ];
+    const postIt = [ 
+        { id:1, title: "Remember to check those web sites", links: links }
+    ];
+
+    return (
+        <div>
+        <a href="https://www.google.com">Google</a>
+        { postIt.map((postIt) => {
+            console.log(postIt);
+            return (<PostIt key={postIt.id} title={postIt.title} links={postIt.links} />);
+        })}
+        </div>
+    );
 }
---
+++ aixampleux/client/src/app/postit/postit.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+export default function PostIt(props) {
+    return (
+        <div className="card" style={{ width: "18rem", backgroundColor: "yellow" }}>
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{props.subtitle}</h6>
+                <p className="card-text">{props.text}</p>
+
+                {props.links.map((link) => (
+                    <a key={link.id} href={link.url} className="card-link">
+                        {link.text}
+                    </a>
+                ))}
+            </div>
+        </div>
+    );
+}
