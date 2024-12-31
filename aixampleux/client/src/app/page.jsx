b8f7911d44f31ef35b3b176cd20c7f99ba4e3ec7
mana-byte <manaikilaut@gmail.com>
2024-12-31 16:31:38 +0100

    added(page.jsx): converted index.html into the page.jsx

--- aixampleux/client/src/app/page.jsx
+++ aixampleux/client/src/app/page.jsx
@@ -4,62 +4,60 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import styles from "./page.module.css";
 
 const postItCorrect = (postIt) => {
-    const template = {
-        id: -1,
-        title: "",
-        text: "",
-        subtitle: "",
-        link: "",
-        linkText: "",
-    };
-    return {
-        id: typeof postIt.id === "number" ? postIt.id : template.id,
-        title: typeof postIt.title === "string" ? postIt.title : template.title,
-        text: typeof postIt.text === "string" ? postIt.text : template.text,
-        subtitle:
-            typeof postIt.subtitle === "string" ? postIt.subtitle : template.subtitle,
-        link: typeof postIt.link === "string" ? postIt.link : template.link,
-        linkText:
-            typeof postIt.linkText === "string" ? postIt.linkText : template.linkText,
-    };
+
+	const template = {
+		id: -1,
+		title: "",
+		text: "",
+		subtitle: "",
+		link: "",
+		linkText: "",
+	};
+	return {
+		id: typeof postIt.id === "number" ? postIt.id : template.id,
+		title: typeof postIt.title === "string" ? postIt.title : template.title,
+		text: typeof postIt.text === "string" ? postIt.text : template.text,
+		subtitle:
+			typeof postIt.subtitle === "string" ? postIt.subtitle : template.subtitle,
+		link: typeof postIt.link === "string" ? postIt.link : template.link,
+		linkText:
+			typeof postIt.linkText === "string" ? postIt.linkText : template.linkText,
+	};
 };
 
 export default async function Index() {
-    const postIt = await fetch("http://localhost:4444/PostIts", {
-        cache: "no-store",
-    }).then((res) =>
-        res.json(),
-    );
+	const postIt = await fetch("http://localhost:4444/PostIts", {
+		cache: "no-store",
+	}).then((res) => res.json());
 
-    return (
-
-        <div className={styles.myPageContainer}>
-            <div className={styles.navbar}>
-                <h1 className={styles.navTitle}>gestion des post-its</h1>
-            </div>
-            <div className={styles.mainContainer}>
-                <div className={styles.cardContainer}>
-                {postIt.map((postIt) => {
-                    const postItCorrected = postItCorrect(postIt);
-                    try {
-                        return (
-                            <PostIt
-                                key={postItCorrected.id}
-                                id={postItCorrected.id}
-                                title={postItCorrected.title}
-                                text={postItCorrected.text}
-                                subtitle={postItCorrected.subtitle}
-                                link={postItCorrected.link}
-                                linkText={postItCorrected.linkText}
-                            />
-                        );
-                    } catch (e) {
-                        console.log(e);
-                    }
-                })}
-                </div>
-            <FormAdd />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.myPageContainer}>
+			<div className={styles.navbar}>
+				<h1 className={styles.navTitle}>gestion des post-its</h1>
+			</div>
+			<div className={styles.mainContainer}>
+				<div className={styles.cardContainer}>
+					{postIt.map((postIt) => {
+						const postItCorrected = postItCorrect(postIt);
+						try {
+							return (
+								<PostIt
+									key={postItCorrected.id}
+									id={postItCorrected.id}
+									title={postItCorrected.title}
+									text={postItCorrected.text}
+									subtitle={postItCorrected.subtitle}
+									link={postItCorrected.link}
+									linkText={postItCorrected.linkText}
+								/>
+							);
+						} catch (e) {
+							console.log(e);
+						}
+					})}
+				</div>
+				<FormAdd />
+			</div>
+		</div>
+	);
 }
--- aixampleux/client/src/app/postit/[id]/form.jsx
+++ aixampleux/client/src/app/postit/[id]/form.jsx
@@ -1,96 +1,100 @@
 "use client";
 import { useState } from "react";
+import { useReportWebVitals } from "next/web-vitals";
 
 export function DeleteForm(props) {
-    const deletePostit = async (e) => {
-        e.preventDefault();
-        // query
-        const rep = await fetch(`http://localhost:4444/api/delete?id=${props.id}`, {
-            method: "DELETE",
-        });
-        console.log(rep);
-        if (rep.status === 200) {
-            window.location.replace("/");
-        }
-    };
+	useReportWebVitals((metric) => {
+		console.log(metric);
+	});
+	const deletePostit = async (e) => {
+		e.preventDefault();
+		// query
+		const rep = await fetch(`http://localhost:4444/api/delete?id=${props.id}`, {
+			method: "DELETE",
+		});
+		console.log(rep);
+		if (rep.status === 200) {
+			window.location.replace("/");
+		}
+	};
 
-    return (
-        <div>
-            <button onClick={deletePostit} type="button">
-                Delete
-            </button>
-        </div>
-    );
+	return (
+		<div>
+			<button onClick={deletePostit} type="button">
+				Delete
+			</button>
+		</div>
+	);
 }
 
 export default function UpdateForm(props) {
-    const [title, setTitle] = useState(props.title);
-    const [text, setText] = useState(props.text);
-    const [subtitle, setSubtitle] = useState(props.subtitle);
-    const [link, setLink] = useState(props.link);
-    const [linkText, setLinkText] = useState(props.linkText);
+	const [title, setTitle] = useState(props.title);
+	const [text, setText] = useState(props.text);
+	const [subtitle, setSubtitle] = useState(props.subtitle);
+	const [link, setLink] = useState(props.link);
+	const [linkText, setLinkText] = useState(props.linkText);
 
-    const updatePost = async (e) => {
-        e.preventDefault();
-        const rep = await fetch("http://localhost:4444/api/update", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                id: props.id,
-                title: title,
-                text: text,
-                subtitle: subtitle,
-                link: link,
-                linkText: linkText,
-            }),
-        });
-        if (rep.status === 200) {
-            window.location.replace(`/postit/${props.id}`);
-        }
-    };
+	const updatePost = async (e) => {
+		e.preventDefault();
+		const rep = await fetch("http://localhost:4444/api/update", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				id: props.id,
+				title: title,
+				text: text,
+				subtitle: subtitle,
+				link: link,
+				linkText: linkText,
+			}),
+		});
+		if (rep.status === 200) {
+			window.location.replace(`/postit/${props.id}`);
+		}
+	};
 
-    return (
-        <div
-            className="card postIt"
-            style={{ width: "18rem", backgroundColor: "yellow" }}
-        >
-            <form onSubmit={updatePost} className="center">
-                <h4>
-                    <strong>new Post It </strong>
-                </h4>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Subtitle"
-                    value={subtitle}
-                    onChange={(e) => setSubtitle(e.target.value)}
-                />
-                <textarea
-                    placeholder="content"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Link"
-                    value={link}
-                    onChange={(e) => setLink(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Text to display for link"
-                    value={linkText}
-                    onChange={(e) => setLinkText(e.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+	return (
+		<div
+			className="card postIt"
+			style={{ width: "18rem", backgroundColor: "yellow" }}
+		>
+			<form onSubmit={updatePost} className="center">
+				<h4>
+					<strong>new Post It </strong>
+				</h4>
+				<input
+					type="text"
+					placeholder="Title"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<input
+					type="text"
+					placeholder="Subtitle"
+					value={subtitle}
+					onChange={(e) => setSubtitle(e.target.value)}
+				/>
+				<textarea
+					placeholder="content"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
+				<input
+					type="text"
+					placeholder="Link"
+					value={link}
+					onChange={(e) => setLink(e.target.value)}
+				/>
+				<input
+					type="text"
+					placeholder="Text to display for link"
+					value={linkText}
+					onChange={(e) => setLinkText(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
